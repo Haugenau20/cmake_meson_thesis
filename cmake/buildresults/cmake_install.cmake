d7890464128c01d0c022842baf0e9b91b0e0de3b
Haugenau20 <soeren@haug.dk>
2023-10-23 14:10:50 +0200

    adding LINAK pc output and hex

--- cmake/buildresults/cmake_install.cmake
+++ cmake/buildresults/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/CMake_Repeatability_test_laptop")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/CMake_Repeatability_test_LINAK_pc")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
