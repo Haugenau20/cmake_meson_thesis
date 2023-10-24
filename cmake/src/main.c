41f526d14db80ca40be580bfdfede9274d960fa1
Haugenau20 <soeren@haug.dk>
2023-10-24 17:54:35 +0200

    EOD commit, deleted unneeded arch dirs and removed them from include paths

--- cmake/src/main.c
+++ cmake/src/main.c
@@ -4,23 +4,28 @@
 
 int main(void)
 {
-    int a = 0;
-    int b = 0;
-    int c = 0;
+    while(1)
+    {
+        int a = 0;
+        int b = 0;
+        int c = 0;
 
-    a = repeatNtimes(5);
-    b = repeatNtimes(10);
+        a = repeatNtimes(5);
+        b = repeatNtimes(10);
 
-    c = add(a, b);
-    c = subtract(c, a);
-    c = multiply(a, a);
-    c = divide(c, a);
-    c = square(a);
-    c = cube(a);
+        c = add(a, b);
+        c = subtract(c, a);
+        c = multiply(a, a);
+        c = divide(c, a);
+        c = square(a);
+        c = cube(a);
 
-    c = subtract_new(c, a);
-    c = multiply_new(a, b);
-    c = divide_new(c, c);
-    c = square_new(b);
-    c = cube_new(c);
+        c = subtract_new(c, a);
+        c = multiply_new(a, b);
+        c = divide_new(c, c);
+        c = square_new(b);
+        c = cube_new(c);
+    }
+
+    return 0;
 }
